6118d5120f0c2b7c9ac0a156abb2de4293a6dc0e
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-15 14:26:35 +0530

    Delete example1.jsx

--- example1.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class Example1 extends Component {
-    state = {  } 
-    render() { 
-        return (<h1>Welcome to React js</h1>);
-    }
-}
- 
-export default Example1;
