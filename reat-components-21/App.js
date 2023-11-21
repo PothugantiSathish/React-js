612e521eda4b8a8b550cbd308f0aba43ddf87657
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-21 14:08:57 +0530

    Add files via upload

---
+++ reat-components-21/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Mycomponents from './reat-components/mycomponents';
+import Color from './reat-components/colors';
+
+
+function App() {
+  return (
+  <div>
+  <Mycomponents/>
+  <Color/>
+  </div> 
+  );
+}
+
+export default App;
