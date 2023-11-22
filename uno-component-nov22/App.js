296a3e11a9c9ec542408cf47919dcc4a8472d84e
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-22 15:11:18 +0530

    Update App.js

--- uno-component-nov22/App.js
+++ uno-component-nov22/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Mycomponents from './reat-components/mycomponents';
 import Color from './reat-components/colors';
 import Table from './uno-component-nov22/table';
+import Image from './uno-component-nov22/image';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   <Mycomponents/>
   <Color/>
   <Table/>
+  <Image/>
   </div> 
   );
 }
