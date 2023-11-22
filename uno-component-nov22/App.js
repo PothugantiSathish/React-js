7d6f0776993c23ff81f71dd1469e8caaac765ac3
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-22 12:39:14 +0530

    Update App.js

--- uno-component-nov22/App.js
+++ uno-component-nov22/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Mycomponents from './reat-components/mycomponents';
 import Color from './reat-components/colors';
+import Table from './uno-component-nov22/table';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   <div>
   <Mycomponents/>
   <Color/>
+  <Table/>
   </div> 
   );
 }
