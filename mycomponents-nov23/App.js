713186b4a116121622c4ac1b0b0d2a5f4e356037
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-23 16:56:56 +0530

    Add files via upload

---
+++ mycomponents-nov23/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Components from './mycomponents-nov23/components';
+import Second from './mycomponents-nov23/second';
+import TableDelate from './mycomponents-nov23/tableDelate';
+function App() {
+  return (
+    <div>
+      <Components/>
+      <Second/>
+      <Second/>
+      <Second/>
+      <TableDelate/>
+    </div>
+    
+  );
+}
+
+export default App;
