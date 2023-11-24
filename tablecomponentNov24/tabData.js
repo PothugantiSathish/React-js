66ceafeb188a53931160d027ba6a85045a149879
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-24 15:45:45 +0530

    Add files via upload

---
+++ tablecomponentNov24/tabData.js
@@ -0,0 +1,35 @@
+const emps=[
+    {
+        "id": 201,
+        "name": "Sathish",
+        "dept": "IT",
+        "salary":35000
+    },
+    {
+        "id": 202,
+        "name": "Anil",
+        "dept": "Sales",
+        "salary":20000
+    },
+    {
+        "id": 203,
+        "name": "Naveen",
+        "dept": "Admin",
+        "salary":15000
+    },
+    {
+        "id": 203,
+        "name": "Ram",
+        "dept": "ceo",
+        "salary":215000
+    },
+    {
+        "id": 204,
+        "name": "Raj",
+        "dept": "IT",
+        "salary":25000
+    },
+]
+export function getemployee(){
+    return emps
+}
