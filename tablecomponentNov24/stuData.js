6a5a72cf6cc5b76cdbe8518153985fdd82a0bf3a
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-24 16:39:49 +0530

    Add files via upload

---
+++ tablecomponentNov24/stuData.js
@@ -0,0 +1,36 @@
+const student=[
+    {
+        "name":"Sathish",
+        "maths":34,
+        "science":70,
+        "social":80
+    },
+    {
+        "name":"naveen",
+        "maths":85,
+        "science":75,
+        "social":40
+    },
+    {
+        "name":"Ram",
+        "maths":60,
+        "science":50,
+        "social":30
+    },
+    {
+        "name":"Anil",
+        "maths":45,
+        "science":49,
+        "social":90
+    },
+    {
+        "name":"Raj",
+        "maths":38,
+        "science":39,
+        "social":49
+    },
+]
+export function getstudent(){
+    return student
+}
+   
