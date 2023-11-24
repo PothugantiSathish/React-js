11e24868f9966187a3d2f23544fd1653b8f5fb6d
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-24 15:21:03 +0530

    Add files via upload

---
+++ tablecomponentNov24/service/tableData.js
@@ -0,0 +1,27 @@
+const emps=[
+    {
+        "id": 201,
+        "name": "Sathish",
+        "dept": "IT",
+        "salary":35000
+    },
+    {
+        "id": 202,
+        "name": "Anil",
+        "dept": "Sales",
+        "salary":20000
+    },
+    {
+        "id": 203,
+        "name": "Naveen",
+        "dept": "Admin",
+        "salary":15000
+    },
+    {
+        "id": 203,
+        "name": "Ram",
+        "dept": "ceo",
+        "salary":215000
+    },
+]
+export default emps
