390ddb255759579ca16186d75207642bfa7db40c
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-11-24 17:09:01 +0530

    Add files via upload

---
+++ tablecomponentNov24/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import Table from './tablecomponentNov24/table';
+import Student from './tablecomponentNov24/student';
+function App() {
+  return (
+    <div>
+      <Student/>
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
