17dbca853ccc24033691a86c80635eeaa3deda1e
PothugantiSathish <146062619+PothugantiSathish@users.noreply.github.com>
2023-12-05 13:04:48 +0530

    Add files via upload

---
+++ components-dec05/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './components-dec05/loginForm';
+
+function App() {
+  return (
+    <LoginForm/>
+  );
+}
+
+export default App;
